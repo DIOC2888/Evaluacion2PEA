0f293b7836fa7d495c9e939961113d7067afbd2a
avdenisse <avrildquezadam@gmail.com>
2026-09-22 19:10:53 -0600

    repository

---
+++ src/main/java/org/example/evaluacion2/Repository/ClientesRepository.java
@@ -0,0 +1,35 @@
+package org.example.evaluacion2.Repository;
+
+import org.example.evaluacion2.model.Clientes;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class ClientesRepository {
+
+    public List<Clientes> findAll(){
+        return List.of(
+                new Clientes(
+                        "Denis Ortega",
+                        "Felino ",
+                        "Managua",
+                        LocalDate.of(2026,9,22),
+                        "Esterilización"
+                ),
+                new Clientes(
+                        "Joshua Donaire",
+                        "Aviar",
+                        "Matagalpa",
+                        LocalDate.of(2026,7,22),
+                        "Grooming"
+                ),
+                new Clientes(
+                        "Avril Quezada",
+                        "Canino",
+                        "Managua",
+                        LocalDate.of(2026,8,22),
+                        "Vacunas"
+                )
+        );
+    }
+}
