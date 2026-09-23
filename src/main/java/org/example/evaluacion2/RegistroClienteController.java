9840e54183b8887b05c61cdd4efd73e64d56c2a6
DIOC2888 <denisivanorcas@gmail.com>
2026-09-22 18:54:11 -0600

    Resgistro cliente

---
+++ src/main/java/org/example/evaluacion2/RegistroClienteController.java
@@ -0,0 +1,42 @@
+package org.example.evaluacion2;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.image.ImageView;
+
+public class RegistroClienteController {
+    @FXML
+    private TextField txtNombres;
+    @FXML
+    private TextField txtApellidos;
+    @FXML
+    private ComboBox<String> cmbTipoCliente;
+    @FXML
+    private ComboBox<String> cmbCiudad;
+    @FXML
+    private DatePicker dtpFechaNacimiento;
+    @FXML
+    private CheckBox chbkGrooming;
+    @FXML
+    private CheckBox chbkEsterilizacion;
+    @FXML
+    private CheckBox chbkVacunas;
+    @FXML
+    private RadioButton rbtnHospitalizacion;
+    @FXML
+    private RadioButton rbtnConsulta;
+    @FXML
+    private ImageView imgFoto;
+    @FXML
+    private Button btnGuardar;
+    @FXML
+    private Button btnLimpiar;
+    @FMX
+
+    private intialize(){
+
+
+    }
+
+
+}
